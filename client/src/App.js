9dfb538c93b6c2147427538e2fa760e4bcea537f
janecyyu <janecyyu@gmail.com>
2020-04-10 08:58:18 -0700

    add api data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      country: "",
+      data: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then((response) => {
+        this.setState({
+          data: response.data,
+        });
+      })
+      .catch((error) => console.log("error" + error));
+  }
+
+  render() {
+    //console.log(this.state.data);
+    return (
+      <div className="App">
+        <h1>my App</h1>
+        <div className="Wrapper">
+          <form>
+            <div className="Input">
+              <input
+                className="Input-text"
+                id="input"
+                name="inputText"
+                // onChange={handleChanges}
+                placeholder="Enter Player's Name"
+                type="text"
+                // value={inputText}
+              />
+              <label htmlFor="input" className="Input-label">
+                Looking for Id
+              </label>
+            </div>
+          </form>
+        </div>
+        <div className="player">
+          {this.state.data.map((person) => (
+            <ul>
+              <li>{person.name}</li>
+              <li>{person.country}</li>
+            </ul>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
